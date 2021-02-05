a730d4a0002c8406e0d0188c2d8c16cfb6b42e5a
Marco Guidi <mark.88g@gmail.com>
2021-02-05 16:46:47 +0100

    + component Pokemon

---
+++ src/components/pages/Pokemon.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    Ciao
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Pokemon",
+        props: {
+            
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
