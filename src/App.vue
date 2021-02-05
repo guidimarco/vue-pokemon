416ac76b86c0434ac7ee68128e50926deb1ca3f6
Marco Guidi <mark.88g@gmail.com>
2021-02-05 16:17:33 +0100

    + install bootstrap + navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-  </div>
+    <div id="app">
+        <NavBar />
+        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import NavBar from "./components/NavBar.vue";
 
-export default {
-  name: "App",
-  components: {
-
-  }
-};
+    export default {
+      name: "App",
+      components: {
+        NavBar
+      }
+    };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "./assets/scss/app.scss";
+
 </style>
